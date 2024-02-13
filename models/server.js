acebf1e8a37ee2ae4bc1d80e859ee3904ca751c2
luismoyano <82573015+lmoyansa@users.noreply.github.com>
2024-02-13 20:48:23 +0100

    default port update

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 8080;
 
         //Paths
         this.path_users = '/api/users'
